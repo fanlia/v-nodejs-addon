9fb366c073d7fe0c954d26b5d87e9592a7a4f78a
odootest <odootest2023@163.com>
2024-01-15 08:32:08 +0800

    first commit

--- binding.v
+++ binding.v
@@ -1,34 +1,34 @@
-
 module main
+
 import napi
 
 @[export: 'napi_register_module_v1']
 fn napi_register_module_v1(env napi.Napi_env, exports napi.Napi_value) napi.Napi_value {
-    mut function := unsafe { nil }
-    if C.napi_create_function(env, unsafe { nil }, 0, foo, unsafe { nil }, &function) != .napi_ok {
-        C.napi_throw_error(env, unsafe { nil }, c"Failed to create function")
-        return unsafe { nil }
-    }
+	mut function := unsafe { nil }
+	if C.napi_create_function(env, unsafe { nil }, 0, foo, unsafe { nil }, &function) != .napi_ok {
+		C.napi_throw_error(env, unsafe { nil }, c'Failed to create function')
+		return unsafe { nil }
+	}
 
-    if C.napi_set_named_property(env, exports, c"foo", function) != .napi_ok {
-        C.napi_throw_error(env, unsafe { nil }, c"Failed to add function to exports")
-        return unsafe { nil }
-    }
+	if C.napi_set_named_property(env, exports, c'foo', function) != .napi_ok {
+		C.napi_throw_error(env, unsafe { nil }, c'Failed to add function to exports')
+		return unsafe { nil }
+	}
 
-    return exports
+	return exports
 }
 
 fn add(a int, b int) int {
-  return a * b
+	return a * b
 }
 
 fn foo(env napi.Napi_env, info napi.Napi_callback_info) napi.Napi_value {
-    mut result := unsafe { nil }
-    value := add(4, 6)
-    if C.napi_create_int32(env, value, &result) != .napi_ok {
-        C.napi_throw_error(env, unsafe { nil }, c"Failed to create return value")
-        return unsafe { nil }
-    }
+	mut result := unsafe { nil }
+	value := add(4, 6)
+	if C.napi_create_int32(env, value, &result) != .napi_ok {
+		C.napi_throw_error(env, unsafe { nil }, c'Failed to create return value')
+		return unsafe { nil }
+	}
 
-    return result
+	return result
 }
--- modules/napi/napi.v
+++ modules/napi/napi.v
@@ -1,11 +1,10 @@
-
 module napi
 
 type Napi_env = voidptr
 type Napi_value = voidptr
 type Napi_callback_info = voidptr
 
-type Napi_callback = fn (env Napi_env, info Napi_callback_info) Napi_value 
+type Napi_callback = fn (env Napi_env, info Napi_callback_info) Napi_value
 
 fn C.napi_create_function(env Napi_env, utf8name &i8, length usize, cb Napi_callback, data voidptr, result &Napi_value) Napi_status
 
@@ -16,5 +15,27 @@ fn C.napi_set_named_property(env Napi_env, object Napi_value, utf8name &i8, valu
 fn C.napi_create_int32(env Napi_env, value int, result &Napi_value) Napi_status
 
 pub enum Napi_status {
-	napi_ok	napi_invalid_arg	napi_object_expected	napi_string_expected	napi_name_expected	napi_function_expected	napi_number_expected	napi_boolean_expected	napi_array_expected	napi_generic_failure	napi_pending_exception	napi_cancelled	napi_escape_called_twice	napi_handle_scope_mismatch	napi_callback_scope_mismatch	napi_queue_full	napi_closing	napi_bigint_expected	napi_date_expected	napi_arraybuffer_expected	napi_detachable_arraybuffer_expected	napi_would_deadlock	napi_no_external_buffers_allowed}
-
+	napi_ok
+	napi_invalid_arg
+	napi_object_expected
+	napi_string_expected
+	napi_name_expected
+	napi_function_expected
+	napi_number_expected
+	napi_boolean_expected
+	napi_array_expected
+	napi_generic_failure
+	napi_pending_exception
+	napi_cancelled
+	napi_escape_called_twice
+	napi_handle_scope_mismatch
+	napi_callback_scope_mismatch
+	napi_queue_full
+	napi_closing
+	napi_bigint_expected
+	napi_date_expected
+	napi_arraybuffer_expected
+	napi_detachable_arraybuffer_expected
+	napi_would_deadlock
+	napi_no_external_buffers_allowed
+}
